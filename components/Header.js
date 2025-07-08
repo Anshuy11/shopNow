34cc81c48a20ccfbfa0eaf91e05904ed49debf75
Anshu <anshuy115@gmail.com>
2025-07-08 09:23:40 +0530

    added global seach component with debounce logic

--- components/Header.js
+++ components/Header.js
@@ -1,91 +1,83 @@
 import { useContext, useState } from "react";
-import {ThemeColor} from "@/pages/theme_context";
+import { ThemeColor } from "@/pages/theme_context";
 
-
-const Header = () => {
+const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [open, setOpen] = useState(false);
-const { theme, ToggleFunc } =useContext(ThemeColor)
-
+  const { theme, ToggleFunc } = useContext(ThemeColor);
 
   return (
-    <div className={theme === "Light" ? "   bg-[#FF6D00] to-headerColor relative  h-[100px] text-white flex  justify-between  " :
-        "   bg-black  h-[100px] text-white flex  justify-between relative "
-        }>
-          <div className="flex justify-start gap-8">
-            <div
-        
-              className="flex justify-start items-start gap-6"
-            >
-              <div className="text-white mt-3  font-semibold sm:text-[14px] text-[12px] flex gap-1 cursor-pointer ">
-                <img
-                  className="h-[100px] w-[100px] object-contain -mt-2 "
-                  src="/logo.png"
-                />
-              </div>
-            </div>
-        
+    <div
+      className={
+        theme === "Light"
+          ? "   bg-[#FF6D00] to-headerColor relative  h-[100px] text-white flex  justify-between  "
+          : "   bg-black  h-[100px] text-white flex  justify-between relative "
+      }
+    >
+      <div className="flex justify-start gap-8">
+        <div className="flex justify-start items-start gap-6">
+          <div className="text-white mt-3  font-semibold sm:text-[14px] text-[12px] flex gap-1 cursor-pointer ">
+            <img
+              className="h-[100px] w-[100px] object-contain -mt-2 "
+              src="/logo.png"
+            />
           </div>
+        </div>
+      </div>
+     
 
-          <div className="flex  items-center pl-3 gap-4">
- <div
-        
-              className="flex justify-start items-start gap-6"
-            >
-              <div className="text-white mt-3  font-semibold sm:text-[14px] text-[12px] flex gap-1 cursor-pointer ">
-                <img
-                  className="h-[100px] w-[100px] object-contain -mt-2 "
-                  src="/shopping-cart.png"
-                />
-              </div>
+      <div className="flex  items-center pl-3 gap-4">
+        <div className="flex justify-start items-start gap-6">
+          <div className="text-white mt-3  font-semibold sm:text-[14px] text-[12px] flex gap-1 cursor-pointer ">
+            <img
+              className="h-[100px] w-[100px] object-contain -mt-2 "
+              src="/shopping-cart.png"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col items-center pl-3 mt-12">
+          <div
+            className={`relative w-12 h-6 rounded-full border transition-all duration-500 ease-in-out ${
+              theme === "Light"
+                ? "bg-white border-gray-300"
+                : "bg-white border-gray-300"
+            }`}
+          >
+            <div
+              onClick={ToggleFunc}
+              className={`absolute top-0.5 h-5 w-5 rounded-full cursor-pointer transition-all duration-500 ease-in-out
+        ${
+          theme === "Light"
+            ? "translate-x-1 bg-black"
+            : "translate-x-6 bg-[#FF6D00]"
+        }`}
+            ></div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center mt-2">
+          <div
+            onMouseEnter={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
+            className="relative inline-block"
+          >
+            <div className="flex justify-center items-center   cursor-pointer mt-3 font-semibold text-[12px] ">
+              <button className="text-center pr-1">User Name</button>
             </div>
-
-                  <div  className="flex flex-col items-center pl-3 mt-12">
-                {theme === "Light"
-                  ?
-                  <div className=" bg-white   text-[14px] font-semibold items-end justify-end flex h-[25px] rounded-md w-7 border  border-white" >
-             
-                  <div  onClick={ToggleFunc} className="cursor-pointer mt-[1px]    text-[14px] font-semibold items-end justify-end flex h-6 rounded-md w-5 bg-black border border-white ">
-                    
-             </div>
-                  </div>
-                  :
-                   <div  className=" bg-white    text-[14px] font-semibold items-end justify-start flex h-[25px] rounded-md w-7 border  border-white">
-             
-                  <div  onClick={ToggleFunc} className="cursor-pointer mt-[1px]  text-[14px] font-semibold items-end justify-start flex h-6 rounded-md w-5 bg-[#FF6D00] to-headerColor border border-white  ">
-                    
-             </div>
-                  </div> 
-                
-                }
-
-              </div>
-                <div className="flex flex-col items-center mt-2">
-                  <div
-                    onMouseEnter={() => setIsOpen(true)}
-                    onMouseLeave={() => setIsOpen(false)}
-                    className="relative inline-block"
-                  >
-                    <div className="flex justify-center items-center   cursor-pointer mt-3 font-semibold text-[12px] ">
-                      <button className="text-center pr-1">User Name</button>
-                    </div>
-                  
-                  </div>
-                  <div className="text-white font-semibold text-[14px] pr-1 mt-2">
-                    <div
-                    
-                      onClick={() => setOpen(true)}
-                      className="w-8 h-8 cursor-pointer"
-                    >
-                      <img src="/power-off.png" />
-                      {/*  <p className="text-[14px]">Logout</p> */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-      
+          </div>
+          <div className="text-white font-semibold text-[14px] pr-1 mt-2">
+            <div
+              onClick={() => setOpen(true)}
+              className="w-8 h-8 cursor-pointer"
+            >
+              <img src="/power-off.png" />
+              {/*  <p className="text-[14px]">Logout</p> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,46 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import GlobalSearch from "@/components/GlobalSearch";
+import React, { useEffect, useMemo, useState } from "react";
 
 const index = () => {
-    const [productsList, setProductsList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    //to call api as soon as page loads
-    useEffect(()=>{
-        ProductListFunc()
-    },[])
-    const ProductListFunc=()=>{
-        setLoading(true)
-        fetch('https://fakestoreapi.com/products')
-        .then((response)=>response.json())
-        .then((result)=>setProductsList(result))
-        .catch((err)=>console.error(err))
-        .finally(()=>setLoading(false))
-    }
-    console.log(productsList,"productList")
+  const [productsList, setProductsList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+
+  // Debounce logic
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, 300); // 300ms debounce time
+
+    return () => clearTimeout(timer); // Clear timeout on unmount or query change
+  }, [query]);
+
+  //to call api as soon as page loads
+  useEffect(() => {
+    ProductListFunc();
+  }, []);
+  // fetch api data
+  const ProductListFunc = () => {
+    setLoading(true);
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((result) => setProductsList(result))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  };
+  // filter logic for universal search with debounce logic
+  const filteredData = useMemo(() => {
+    if (!debouncedQuery) return productsList;
+
+    return productsList.filter((item) =>
+      Object.values(item).some((value) =>
+        value?.toString().toLowerCase().includes(debouncedQuery.toLowerCase())
+      )
+    );
+  }, [debouncedQuery, productsList]);
+
   return (
     <>
-       <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">Product Listings</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {productsList.map(product => (
-         <div 
-  key={product.id}
-  className="border rounded-xl shadow-md p-4 transition-all duration-500 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-105 hover:shadow-xl hover:-translate-y-1 cursor-pointer"
->
-  <img
-    src={product.image}
-    alt={product.title}
-    className="h-24 object-contain w-full mb-4"
-  />
-  <h4 className="font-semibold text-lg mb-2">{product.title}</h4>
-  <h5 className="text-gray-600 mb-2 text-[20px]">${product.price}</h5>
-  <div className="text-[6px] text-gray-500 line-clamp-2 ">{product.description}</div>
-</div>
+      <div className="p-6">
+        <div className="w-full">
+          <GlobalSearch
+            filteredData={filteredData}
+            setQuery={setQuery}
+            query={query}
+          />
+        </div>
 
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
+          {filteredData.map((product) => (
+            <div
+              key={product.id}
+              className="border rounded-xl shadow-md p-4 transition-all duration-500 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-105 hover:shadow-xl hover:-translate-y-1 cursor-pointer"
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                className="h-24 object-contain w-full mb-4"
+              />
+              <h4 className="font-semibold text-lg mb-2">{product.title}</h4>
+              <h5 className="text-gray-600 mb-2 text-[20px]">
+                ${product.price}
+              </h5>
+              <div className="text-[6px] text-gray-500 line-clamp-2 ">
+                {product.description}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default index
+export default index;
