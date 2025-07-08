823d363f27f9f39207dfb8cf574b4c7bf0e5e361
Anshu <anshuy115@gmail.com>
2025-07-08 09:29:27 +0530

    cart design added

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const Header = (props) => {
           ? "   bg-[#FF6D00] to-headerColor relative  h-[100px] text-white flex  justify-between  "
           : "   bg-black  h-[100px] text-white flex  justify-between relative "
       }
-    >
+    >{/* Logo section */}
       <div className="flex justify-start gap-8">
         <div className="flex justify-start items-start gap-6">
           <div className="text-white mt-3  font-semibold sm:text-[14px] text-[12px] flex gap-1 cursor-pointer ">
@@ -24,17 +24,27 @@ const Header = (props) => {
           </div>
         </div>
       </div>
-     
 
       <div className="flex  items-center pl-3 gap-4">
-        <div className="flex justify-start items-start gap-6">
-          <div className="text-white mt-3  font-semibold sm:text-[14px] text-[12px] flex gap-1 cursor-pointer ">
-            <img
-              className="h-[100px] w-[100px] object-contain -mt-2 "
-              src="/shopping-cart.png"
+        {/* Cart section */}
+        <div className="flex justify-start items-start gap-6 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="white"
+            className="w-10 h-10 mt-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.837l.383 1.43m0 0L6.75 12.75m-1.644-7.483h13.488a.75.75 0 01.732.927l-1.5 6a.75.75 0 01-.732.573H7.152m0 0l-.347 1.394a1.5 1.5 0 001.458 1.856h10.239m-11.697 0a1.125 1.125 0 110 2.25m11.25-2.25a1.125 1.125 0 110 2.25"
             />
-          </div>
+          </svg>
         </div>
+
+        {/* Dual theme section */}
         <div className="flex flex-col items-center pl-3 mt-12">
           <div
             className={`relative w-12 h-6 rounded-full border transition-all duration-500 ease-in-out ${
@@ -54,7 +64,7 @@ const Header = (props) => {
             ></div>
           </div>
         </div>
-
+{/* User Information section */}
         <div className="flex flex-col items-center mt-2">
           <div
             onMouseEnter={() => setIsOpen(true)}
