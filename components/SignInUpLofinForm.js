4ff97c5b9daaa5a7f7194d2d575a1c79f678267b
Anshu <anshuy115@gmail.com>
2025-08-02 21:16:36 +0530

    sign up model

--- components/SignInUpLofinForm.js
+++ components/SignInUpLofinForm.js
@@ -14,7 +14,6 @@ const SignInUpLofinForm = (props) => {
     name: "",
     password: "",
   });
-  
 
   const cancelButtonRef = useRef(null);
 
@@ -31,26 +30,25 @@ const SignInUpLofinForm = (props) => {
     loginSuccess,
     signUpSuccess,
     setLoginSuccess,
-    setSignUpSuccess
+    setSignUpSuccess,
   } = useContext(AuthContext);
-/* To close or clear the state  */
- useEffect(() => {
- 
-  if (signUpSuccess) {
-    setName("");
-    setEmail("");
-    setPassword("");
-    setSignUpSuccess(false);
-  }
-  
-  if (loginSuccess) {
-    setEmail("");
-    setPassword("");
-    props.setOpen(false);
-    setShowSignin(false);
-    setLoginSuccess(false);
-  }
-}, [signUpSuccess, loginSuccess]);
+  /* To close or clear the state  */
+  useEffect(() => {
+    if (signUpSuccess) {
+      setName("");
+      setEmail("");
+      setPassword("");
+      setSignUpSuccess(false);
+    }
+
+    if (loginSuccess) {
+      setEmail("");
+      setPassword("");
+      props.setOpen(false);
+      setShowSignin(false);
+      setLoginSuccess(false);
+    }
+  }, [signUpSuccess, loginSuccess]);
   /* SignUp func */
   const signUpHandleSubmit = (e) => {
     e.preventDefault();
@@ -70,9 +68,8 @@ const SignInUpLofinForm = (props) => {
     if (!isValid) return;
 
     handleSubmitSignUp({ name, email, password });
-   
   };
-/* Loginfunc */
+  /* Loginfunc */
   const loginHandleSubmit = (e) => {
     e.preventDefault();
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -90,7 +87,6 @@ const SignInUpLofinForm = (props) => {
     if (!isValid) return;
 
     handleSubmitLogin({ email, password });
-   
   };
 
   const handleEmailChange = (e) => {
@@ -127,188 +123,196 @@ const SignInUpLofinForm = (props) => {
   return (
     <>
       <MessageFunc
-                message={messageText}
-                setMessageOpen={setMessageOpen}
-                messageOpen={messageOpen}
-                onClose={() => setMessageOpen(false)}
-              />
-   
-    <Transition.Root show={props.open} as={Fragment} className="p-2 m-2 z-[1000] ">
-      <Dialog
-        as="div"
-        className="relative z-50 sm:block"
-        initialFocus={cancelButtonRef}
-        onClose={() => {
-          props.setOpen(false);
-          setShowSignin(false);
-        }}
+        message={messageText}
+        setMessageOpen={setMessageOpen}
+        messageOpen={messageOpen}
+        onClose={() => setMessageOpen(false)}
+      />
+      <Transition.Root
+        show={props.open}
+        as={Fragment}
+        className="p-2 m-2 z-[1000] "
       >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+        <Dialog
+          as="div"
+          className="relative z-50 sm:block"
+          initialFocus={cancelButtonRef}
+          onClose={() => {
+            props.setOpen(false);
+            setShowSignin(false);
+          }}
         >
-          <div className="fixed inset-0  transition-opacity backdrop-blur-sm" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-50 flex items-center justify-center p-2 m-2">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
             leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Dialog.Panel
-              className={`relative transform rounded-lg border-gray-300 border  shadow-md ${
-                theme == "Light" ? "bg-white" : "bg-gray-700"
-              }  overflow-x-auto w-fit   sm:p-6`}
-            >
-              {/* Your code here */}
-            
-              <div className="flex justify-end items-end gap-6">
-                <div className="mt-2">
-                  <button
-                    type="button"
-                    className="absolute top-3 right-2.5  bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex border border-red-500 dark:hover:bg-red-200 dark:hover:text-white"
-                    onClick={() => {
-                      props.setOpen(false);
-                      setShowSignin(false);
-                    }}
-                    ref={cancelButtonRef}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="#ff002d"
-                      className="w-4 h-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-
-              <div className="flex items-center justify-center h-fit mt-6 ">
-                <form
-                  onSubmit={showSignin ? signUpHandleSubmit : loginHandleSubmit}
-                  className=" p-6 rounded-xl   w-full max-w-sm space-y-4"
-                >
-                  {showSignin && (
-                    <div>
-                      <label className="block  mb-1">Name</label>
-                      <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => handleNameChange(e)}
-                        maxLength={100}
-                        className="w-full px-3 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-300"
-                        placeholder="Enter your name"
-                        required
-                      />{" "}
-                      {errors.name && (
-                        <p className="text-red-400 text-sm mt-1">
-                          {errors.name}
-                        </p>
-                      )}
-                    </div>
-                  )}
-
-                  <div>
-                    <label className="block  mb-1">Email</label>
-                    <input
-                      type="email"
-                      value={email}
-                      onChange={(e) => handleEmailChange(e)}
-                      className="w-full px-3 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-300"
-                      placeholder="Enter your email"
-                      required
-                    />{" "}
-                    {errors.email && (
-                      <p className="text-red-400 text-sm mt-1">
-                        {errors.email}
-                      </p>
-                    )}
-                  </div>
-
-                  <div>
-                    <label className="block  mb-1">Password</label>
-                    <div className="relative">
-                      <input
-                        type={showPassword ? "text" : "password"}
-                        value={password}
-                        minLength={4}
-                        maxLength={50}
-                        onChange={(e) => handlePasswordChange(e)}
-                        className="w-full px-3 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-300 pr-10"
-                        placeholder="Enter your password"
-                        required
-                      />{" "}
-                      {errors.password && (
-                        <p className="text-red-400 text-sm mt-1">
-                          {errors.password}
-                        </p>
-                      )}
-                      <button
-                        type="button"
-                        onClick={() => setShowPassword(!showPassword)}
-                        className="absolute inset-y-0 right-2 flex items-center text-sm  hover:text-gray-500 cursor-pointer focus:outline-none"
-                      >
-                        {showPassword ? "Hide" : "Show"}
-                      </button>
-                    </div>
-                  </div>
-
-                  {signUpLoader ? (
-                    <div className="w-full cursor-none bg-gray-500 text-white py-2 rounded-lg  transition  animate-pulse text-center">
-                      Please wait...
-                    </div>
-                  ) : (
-                    <button
-                      type="submit"
-                      className="w-full cursor-pointer bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-700 transition"
-                    >
-                      {showSignin ? "Sign Up" : "Login"}
-                    </button>
-                  )}
-                  <div className="text-center">
-                     <button
-                       type="submit"
-                      onClick={() => { setEmail("testuser1@gmail.com"); setPassword("Testing@0")}}
-                    className={` p-2  text-blue-500   cursor-pointer  text-center rounded-md } `}
-                  >
-                    Login as Guest
-                  </button>
-
-
-                  </div>
-                  
-                  <div
-                    onClick={() => setShowSignin(!showSignin)}
-                    className={` p-2  text-blue-500  text-center cursor-pointer   rounded-md } `}
-                  >
-                    {showSignin ? "Login" : "Sign Up"}
-                  </div>
-                </form>
-              </div>
-            </Dialog.Panel>
+            <div className="fixed inset-0  transition-opacity backdrop-blur-sm" />
           </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root> </>
 
+          <div className="fixed inset-0 z-50 flex items-center justify-center p-2 m-2">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel
+                className={`relative transform rounded-lg border-gray-300 border  shadow-md ${
+                  theme == "Light" ? "bg-white" : "bg-gray-700"
+                }  overflow-x-auto w-fit   sm:p-6`}
+              >
+                {/* Your code here */}
+
+                <div className="flex justify-end items-end gap-6">
+                  <div className="mt-2">
+                    <button
+                      type="button"
+                      className="absolute top-3 right-2.5  bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex border border-red-500 dark:hover:bg-red-200 dark:hover:text-white"
+                      onClick={() => {
+                        props.setOpen(false);
+                        setShowSignin(false);
+                      }}
+                      ref={cancelButtonRef}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="#ff002d"
+                        className="w-4 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-center h-fit mt-6 ">
+                  <form
+                    onSubmit={
+                      showSignin ? signUpHandleSubmit : loginHandleSubmit
+                    }
+                    className=" p-6 rounded-xl   w-full max-w-sm space-y-4"
+                  >
+                    {showSignin && (
+                      <div>
+                        <label className="block  mb-1">Name</label>
+                        <input
+                          type="text"
+                          value={name}
+                          onChange={(e) => handleNameChange(e)}
+                          maxLength={100}
+                          className="w-full px-3 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-300"
+                          placeholder="Enter your name"
+                          required
+                        />{" "}
+                        {errors.name && (
+                          <p className="text-red-400 text-sm mt-1">
+                            {errors.name}
+                          </p>
+                        )}
+                      </div>
+                    )}
+
+                    <div>
+                      <label className="block  mb-1">Email</label>
+                      <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => handleEmailChange(e)}
+                        className="w-full px-3 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-300"
+                        placeholder="Enter your email"
+                        required
+                      />{" "}
+                      {errors.email && (
+                        <p className="text-red-400 text-sm mt-1">
+                          {errors.email}
+                        </p>
+                      )}
+                    </div>
+
+                    <div>
+                      <label className="block  mb-1">Password</label>
+                      <div className="relative">
+                        <input
+                          type={showPassword ? "text" : "password"}
+                          value={password}
+                          minLength={4}
+                          maxLength={50}
+                          onChange={(e) => handlePasswordChange(e)}
+                          className="w-full px-3 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-300 pr-10"
+                          placeholder="Enter your password"
+                          required
+                        />{" "}
+                        {errors.password && (
+                          <p className="text-red-400 text-sm mt-1">
+                            {errors.password}
+                          </p>
+                        )}
+                        <button
+                          type="button"
+                          onClick={() => setShowPassword(!showPassword)}
+                          className="absolute inset-y-0 right-2 flex items-center text-sm  hover:text-gray-500 cursor-pointer focus:outline-none"
+                        >
+                          {showPassword ? "Hide" : "Show"}
+                        </button>
+                      </div>
+                    </div>
+
+                    {signUpLoader ? (
+                      <div className="w-full cursor-none bg-gray-500 text-white py-2 rounded-lg  transition  animate-pulse text-center">
+                        Please wait...
+                      </div>
+                    ) : (
+                      <button
+                        type="submit"
+                        className="w-full cursor-pointer bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+                      >
+                        {showSignin ? "Sign Up" : "Login"}
+                      </button>
+                    )}
+                    {showSignin == false && (
+                      <div className="text-center">
+                        <button
+                          type="submit"
+                          onClick={() => {
+                            setEmail("testuser1@gmail.com");
+                            setPassword("Testing@0");
+                          }}
+                          className={` p-2  text-blue-500   cursor-pointer  text-center rounded-md } `}
+                        >
+                          Login as Guest
+                        </button>
+                      </div>
+                    )}
+
+                    <div
+                      onClick={() => setShowSignin(!showSignin)}
+                      className={` p-2  text-blue-500  text-center cursor-pointer   rounded-md } `}
+                    >
+                      {showSignin ? "Login" : "Sign Up"}
+                    </div>
+                  </form>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>{" "}
+    </>
   );
 };
 
